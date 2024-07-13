9d7c9f55203f0720cf2f8dd5349b9dc36bc13d04
Tan Nguyen <tannp27@gmail.com>
2024-07-13 10:28:23 +0700

    feat: create base post controller for api package

---
+++ src/Http/Controllers/Base/BasePostController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CSlant\BlogApi\Http\Controllers\Base;
+
+use Botble\Blog\Http\Controllers\API\PostController as BotblePostController;
+
+class BasePostController extends BotblePostController
+{
+}
--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace CSlant\BlogApi\Http\Controllers;
 
-use Botble\Blog\Http\Controllers\API\PostController as BasePostController;
+use CSlant\BlogApi\Http\Controllers\Base\BasePostController;
 
 class PostController extends BasePostController
 {
