179fab7f5ea227a261495051272ec06d406f9d17
Tan Nguyen <tannp27@gmail.com>
2024-12-10 22:35:30 +0700

    #28 feat: Create rate limit for API

---
+++ src/Providers/RouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CSlant\Blog\ApiPackage\Providers;
+
+use Botble\Theme\Facades\Theme;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Move base routes to a service provider to make sure all filters & actions can hook to base routes
+     */
+    public function boot(): void
+    {
+        // add rate limit for api
+        $this->configureRateLimiting();
+    }
+
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for((string) config('blog-api.defaults.route_prefix'), function (Request $request) {
+            return Limit::perMinute(40)->by(optional($request->user())->id ?: $request->ip());
+        });
+    }
+}
