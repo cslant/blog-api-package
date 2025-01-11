7b47c22ad1559f5b8631629001cd25aec9ca7275
Tan Nguyen <tannp27@gmail.com>
2025-01-12 02:35:07 +0700

    feat: update filter post resource

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -10,6 +10,7 @@ use CSlant\Blog\Core\Http\Controllers\Base\BasePostController;
 use CSlant\Blog\Core\Http\Responses\Base\BaseHttpResponse;
 use CSlant\Blog\Core\Models\Post;
 use CSlant\Blog\Core\Models\Slug;
+use CSlant\Blog\Core\Supports\Base\FilterPost;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -95,6 +96,18 @@ class PostController extends BasePostController
         return $this
             ->httpResponse()
             ->setData(new PostResource($post))
+            ->toApiResponse();
+    }
+
+    public function getFilters(Request $request)
+    {
+        $filters = FilterPost::setFilters($request->input());
+
+        $data = $this->postRepository->getFilters($filters);
+
+        return $this
+            ->httpResponse()
+            ->setData(ListPostResource::collection($data))
             ->toApiResponse();
     }
 }
