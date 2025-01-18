f71c8688159d582c44ad5e331e553c4fd613397d
Tan Nguyen <tannp27@gmail.com>
2025-01-18 19:50:31 +0700

    feat: update category resource

--- src/Http/Controllers/CategoryController.php
+++ src/Http/Controllers/CategoryController.php
@@ -2,8 +2,51 @@
 
 namespace CSlant\Blog\Api\Http\Controllers;
 
+use Botble\Blog\Http\Resources\ListCategoryResource;
+use CSlant\Blog\Api\Enums\StatusEnum;
+use CSlant\Blog\Core\Facades\Base\SlugHelper;
 use CSlant\Blog\Core\Http\Controllers\Base\BaseCategoryController;
+use CSlant\Blog\Core\Models\Category;
 
 class CategoryController extends BaseCategoryController
 {
+    /**
+     * Get category by slug
+     *
+     * @group Blog
+     * @queryParam slug Find by slug of category.
+     */
+    public function findBySlug(string $slug)
+    {
+        $slug = SlugHelper::getSlug($slug, SlugHelper::getPrefix(Category::getBaseModel()));
+
+        if (! $slug) {
+            return $this
+                ->httpResponse()
+                ->setError()
+                ->setCode(404)
+                ->setMessage('Not found');
+        }
+
+        $category = Category::query()
+            ->with('slugable')
+            ->where([
+                'id' => $slug->reference_id,
+                'status' => StatusEnum::PUBLISHED,
+            ])
+            ->first();
+
+        if (! $category) {
+            return $this
+                ->httpResponse()
+                ->setError()
+                ->setCode(404)
+                ->setMessage('Not found');
+        }
+
+        return $this
+            ->httpResponse()
+            ->setData(new ListCategoryResource($category))
+            ->toApiResponse();
+    }
 }
---
+++ src/Http/Resources/ListCategoryResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CSlant\Blog\Api\Http\Resources;
+
+use CSlant\Blog\Core\Http\Resources\Base\BaseListCategoryResource;
+use CSlant\Blog\Core\Models\Category;
+use Illuminate\Http\Request;
+
+/**
+ * @mixin Category
+ */
+class ListCategoryResource extends BaseListCategoryResource
+{
+    /**
+     * @param  Request  $request
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        /** @var Category $this */
+
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'icon' => $this->icon,
+            'description' => $this->description,
+            'children' => CategoryResource::collection($this->children),
+            'parent' => new CategoryResource($this->parent),
+        ];
+    }
+}
