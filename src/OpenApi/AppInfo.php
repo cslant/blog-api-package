83dd683e338846804009990388a481994cacc59d
Tan Nguyen <tannp27@gmail.com>
2025-03-08 16:32:21 +0700

    feat: add AppInfo class for OpenAPI documentation

---
+++ src/OpenApi/AppInfo.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CSlant\Blog\Api\OpenApi;
+
+class AppInfo
+{
+    /**
+     * @OA\Info(
+     *     description="The API Documentation for CSlant Blog - Build by cslant.com",
+     *     version="1.0.0",
+     *     title="The API Documentation",
+     *     termsOfService="https://swagger.io/terms/",
+     *     @OA\Contact(
+     *         email="contact@cslant.com",
+     *         name="CSlant",
+     *         url="https://cslant.com"
+     *     ),
+     *     @OA\License(
+     *         name="Apache 2.0",
+     *         url="https://www.apache.org/licenses/LICENSE-2.0.html"
+     *     )
+     * )
+     * @OA\Schemes(
+     *     scheme="http",
+     *     scheme="https",
+     * )
+     * @OA\ExternalDocumentation(
+     *     description="Find out more about Swagger",
+     *     url="https://swagger.io"
+     * )
+     * @OA\PathItem(
+     *     path="/",
+     * )
+     *
+     * @OA\Server(
+     *     url="/cs-api",
+     *     description="The API Base Path"
+     * )
+     *
+     * @OA\SecurityScheme(
+     *     type="http",
+     *     description="Authentication by bearer token",
+     *     name="bearerAuth",
+     *     in="header",
+     *     scheme="bearer",
+     *     securityScheme="sanctum"
+     * )
+     */
+    public function index(): void
+    {
+    }
+}
