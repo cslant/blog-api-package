b73a2d19b689419c2239c667c2045098aac1cb1d
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-11 23:26:21 +0700

    #44 feat: change required for AuthorSchema

--- src/OpenApi/Schemas/Models/AuthorSchema.php
+++ src/OpenApi/Schemas/Models/AuthorSchema.php
@@ -10,7 +10,7 @@ use OpenApi\Attributes\Schema;
     required: ["id", "email"],
     properties: [
         new Property(property: "id", type: "integer", uniqueItems: true),
-        new Property(property: "email", description: "Author email", type: "string", maxLength: 191),
+        new Property(property: "email", description: "Author email", type: "string", maxLength: 191, uniqueItems: true),
         new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120, nullable: true),
         new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120, nullable: true),
         new Property(property: "username", description: "Author username", type: "string", maxLength: 60, uniqueItems: true),
