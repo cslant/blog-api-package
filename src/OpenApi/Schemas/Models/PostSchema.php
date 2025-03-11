560b651f5192eb27204ea57cf7b39e1d01d1a47a
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-11 17:30:03 +0700

    #42 feat: add PostSchema model and extend PostModelResourceScema with category propety

---
+++ src/OpenApi/Schemas/Models/PostSchema.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace CSlant\Blog\Api\OpenApi\Schemas\Models;
+
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Schema;
+
+#[Schema(
+    schema: "Post",
+    required: ["id", "name", "slug", "description", "content", "created_at", "updated_at"],
+    properties: [
+        new Property(property: "id", type: "integer", uniqueItems: true),
+        new Property(property: "name", description: "Post name", type: "string", maxLength: 255),
+        new Property(property: "slug", description: "Post slug", type: "string", maxLength: 255, uniqueItems: true),
+        new Property(property: "description", type: "string", nullable: true),
+        new Property(property: "content", type: "string", nullable: true),
+        new Property(property: "image", type: "string", nullable: true),
+        new Property(
+            property: "categories",
+            type: "array",
+            items: new Items(ref: CategorySchema::class)
+        ),
+        new Property(
+            property: "tags",
+            type: "array",
+            items: new Items(ref: CategorySchema::class)
+        ),
+        new Property(
+            property: "author",
+            ref: CategorySchema::class,
+            type: "object",
+        ),
+        new Property(property: "created_at", type: "datetime", nullable: true),
+        new Property(property: "updated_at", type: "datetime", nullable: true),
+    ],
+    type: "object"
+)]
+class PostSchema
+{
+}
