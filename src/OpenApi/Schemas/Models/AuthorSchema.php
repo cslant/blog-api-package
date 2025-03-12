88041b6a4d82b70c6055a822102fe79d0f2f3e1f
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-12 10:03:51 +0700

    #44 feat: add AuthorModelResourceSchema

--- src/OpenApi/Schemas/Models/AuthorSchema.php
+++ src/OpenApi/Schemas/Models/AuthorSchema.php
@@ -7,13 +7,13 @@ use OpenApi\Attributes\Schema;
 
 #[Schema(
     schema: "Author",
-    required: ["id"],
+    required: ["id", "email"],
     properties: [
         new Property(property: "id", type: "integer", uniqueItems: true),
+        new Property(property: "email", description: "Author email", type: "string", maxLength: 191, nullable: true),
         new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120),
         new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120),
-        new Property(property: "image", description: "Author image", type: "string", maxLength: 255),
-        new Property(property: "role", description: "Author role", type: "string", maxLength: 120),
+        new Property(property: "username", description: "Author username", type: "string", maxLength: 60, uniqueItems: true),
     ],
     type: "object"
 )]
