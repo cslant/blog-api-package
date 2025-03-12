d11c5fff7e8f3c4c00ce70077429e1f45f6fc014
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-12 23:56:05 +0700

    #43 feat: add api docs for post actions

--- src/Http/Controllers/PostController.php
+++ src/Http/Controllers/PostController.php
@@ -6,6 +6,8 @@ use Botble\Base\Http\Responses\BaseHttpResponse;
 use CSlant\Blog\Api\Enums\StatusEnum;
 use CSlant\Blog\Api\Http\Resources\ListPostResource;
 use CSlant\Blog\Api\Http\Resources\PostResource;
+use CSlant\Blog\Api\OpenApi\Schemas\Resources\Post\PostListResourceSchema;
+use CSlant\Blog\Api\OpenApi\Schemas\Resources\Post\PostModelResourceSchema;
 use CSlant\Blog\Core\Facades\Base\SlugHelper;
 use CSlant\Blog\Core\Http\Controllers\Base\BasePostController;
 use CSlant\Blog\Core\Models\Post;
@@ -15,6 +17,12 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Parameter;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
 
 /**
  * Class PostController
@@ -38,6 +46,77 @@ class PostController extends BasePostController
      *
      * @return BaseHttpResponse|JsonResource|JsonResponse|RedirectResponse
      */
+    #[
+        Get(
+            path: "/posts",
+            operationId: "postGetAllWithFilter",
+            description: "Get all posts with pagination (10 items per page by default, page 1 by default)
+            
+    This API will get records from the database and return them as a paginated list. 
+    The default number of items per page is 10 and the default page number is 1. You can change these values by passing the `per_page` and `page` query parameters.
+            ",
+            summary: "Get all posts with pagination",
+            security: [['sanctum' => []]],
+            tags: ["Post"],
+            parameters: [
+                new Parameter(
+                    name: 'per_page',
+                    description: 'Number of items per page',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 10)
+                ),
+                new Parameter(
+                    name: 'page',
+                    description: 'Page number',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 1)
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get posts successfully",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: 'data',
+                                description: 'Data',
+                                properties: [
+                                    new Property(
+                                        property: 'post',
+                                        ref: PostListResourceSchema::class,
+                                        description: 'Post',
+                                        type: 'object'
+                                    ),
+                                ],
+                                type: 'object'
+                            ),
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\InternalServerResponseSchema::class,
+                    response: 500,
+                ),
+            ]
+        )
+    ]
     public function index(Request $request): BaseHttpResponse|JsonResponse|JsonResource|RedirectResponse
     {
         $data = $this
@@ -64,6 +143,62 @@ class PostController extends BasePostController
      * @queryParam slug Find by slug of post.
      * @return BaseHttpResponse|JsonResource|JsonResponse|RedirectResponse
      */
+    #[
+        Get(
+            path: "/posts/{slug}",
+            operationId: "postFilterBySlug",
+            description: "Get the post by slug
+            
+    This API will get records from the database and return the post by slug.
+            ",
+            summary: "Get post by slug",
+            security: [['sanctum' => []]],
+            tags: ["Post"],
+            parameters: [
+                new Parameter(
+                    name: 'slug',
+                    description: 'Post slug',
+                    in: 'path',
+                    required: true,
+                    schema: new Schema(type: 'string', example: 'php')
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get post successfully",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: "data",
+                                ref: PostModelResourceSchema::class,
+                                description: "Data of model",
+                                type: "object",
+                            ),
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\InternalServerResponseSchema::class,
+                    response: 500,
+                ),
+            ]
+        )
+    ]
     public function findBySlug(string $slug): BaseHttpResponse|JsonResponse|JsonResource|RedirectResponse
     {
         /** @var Slug $slug */
@@ -104,6 +239,77 @@ class PostController extends BasePostController
      *
      * @return BaseHttpResponse|JsonResource|JsonResponse|RedirectResponse
      */
+    #[
+        Get(
+            path: "/posts/filters",
+            operationId: "postGetWithFilter",
+            description: "Get all posts with pagination (10 items per page by default, page 1 by default)
+            
+    This API will get records from the database and return them as a paginated list. 
+    The default number of items per page is 10 and the default page number is 1. You can change these values by passing the `per_page` and `page` query parameters.
+            ",
+            summary: "Get posts by filter with pagination",
+            security: [['sanctum' => []]],
+            tags: ["Post"],
+            parameters: [
+                new Parameter(
+                    name: 'per_page',
+                    description: 'Number of items per page',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 10)
+                ),
+                new Parameter(
+                    name: 'page',
+                    description: 'Page number',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 1)
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get posts successfully",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: 'data',
+                                description: 'Data',
+                                properties: [
+                                    new Property(
+                                        property: 'post',
+                                        ref: PostListResourceSchema::class,
+                                        description: 'Post',
+                                        type: 'object'
+                                    ),
+                                ],
+                                type: 'object'
+                            ),
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\InternalServerResponseSchema::class,
+                    response: 500,
+                ),
+            ]
+        )
+    ]
     public function getFilters(Request $request): BaseHttpResponse|JsonResponse|JsonResource|RedirectResponse
     {
         $filters = FilterPost::setFilters($request->input());
