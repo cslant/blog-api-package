6e01324218fae5303d0f535da0fa1283f610319c
FrancisDuong <haoda@cslant.com>
2025-03-12 19:38:54 +0700

    #41 feat: create get all and get by slug for tag actions in the tag controller

--- src/Http/Controllers/TagController.php
+++ src/Http/Controllers/TagController.php
@@ -5,13 +5,24 @@ namespace CSlant\Blog\Api\Http\Controllers;
 use Botble\Base\Http\Responses\BaseHttpResponse;
 use CSlant\Blog\Api\Enums\StatusEnum;
 use CSlant\Blog\Api\Http\Resources\TagResource;
+use CSlant\Blog\Api\OpenApi\Responses\Errors\BadRequestResponseSchema;
+use CSlant\Blog\Api\OpenApi\Responses\Errors\ErrorNotFoundResponseSchema;
+use CSlant\Blog\Api\OpenApi\Responses\Errors\InternalServerResponseSchema;
+use CSlant\Blog\Api\OpenApi\Schemas\Resources\Tag\TagModelResourceSchema;
 use CSlant\Blog\Core\Facades\Base\SlugHelper;
 use CSlant\Blog\Core\Http\Controllers\Base\BaseTagController;
 use CSlant\Blog\Core\Models\Slug;
 use CSlant\Blog\Core\Models\Tag;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Parameter;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
 
 /**
  * Class TagController
@@ -27,6 +38,82 @@ use Illuminate\Http\Resources\Json\JsonResource;
  */
 class TagController extends BaseTagController
 {
+    #[
+        Get(
+            path: "/tags",
+            operationId: "tagGetAllWithFilter",
+            description: "Get all tags with pagination (10 items per page by default, page 1 by default, page 1 by default)
+    This API will get records from the database and return them as a paginated list. 
+    The default number of items per page is 10 and the default page number is 1. You can change these values by passing the `per_page` and `page` query parameters.
+          ",
+            summary: "Get all tags with pagination",
+            security: [['sanctum' => []]],
+            tags: ["Tag"],
+            parameters: [
+                new Parameter(
+                    name: 'per_page',
+                    description: 'Number of items per page',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 10)
+                ),
+                new Parameter(
+                    name: 'page',
+                    description: 'Page number',
+                    in: 'query',
+                    required: false,
+                    schema: new Schema(type: 'integer', default: 1)
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get tags successfully",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: 'data',
+                                description: 'Data',
+                                properties: [
+                                    new Property(
+                                        property: 'tag',
+                                        ref: TagModelResourceSchema::class,
+                                        description: 'Tag',
+                                        type: 'object'
+                                    )
+                                ],
+                                type: 'object'
+                            )
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: InternalServerResponseSchema::class,
+                    response: 500,
+                )
+            ]
+        )
+    ]
+
+    public function index(Request $request): BaseHttpResponse|JsonResponse|JsonResource|RedirectResponse
+    {
+        return parent::index($request);
+    }
+
     /**
      * Get tag by slug
      *
@@ -37,6 +124,62 @@ class TagController extends BaseTagController
      *
      * @return BaseHttpResponse|JsonResource|JsonResponse|RedirectResponse
      */
+    #[
+        Get(
+            path: "/tags/{slug}",
+            operationId: "tagFilterBySlug",
+            description: "Get the tag by slug
+            
+    This API will get records from the database and return the tag by slug.
+            ",
+            summary: "Get tag by slug",
+            security: [['sanctum' => []]],
+            tags: ["Tag"],
+            parameters: [
+                new Parameter(
+                    name: 'slug',
+                    description: 'Tag slug',
+                    in: 'path',
+                    required: true,
+                    schema: new Schema(type: 'string', example: 'php')
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get tag successfully",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: "data",
+                                ref: TagModelResourceSchema::class,
+                                description: "Data of model",
+                                type: "object",
+                            ),
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: InternalServerResponseSchema::class,
+                    response: 500,
+                ),
+            ]
+        )
+    ]
     public function findBySlug(string $slug): JsonResponse|RedirectResponse|JsonResource|BaseHttpResponse
     {
         /** @var Slug $slug */
