d7c9c4d98da578e1530e678fa29c126d4a6c51ef
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-12 10:03:51 +0700

    #44 feat: add AuthorModelResourceSchema

--- src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
+++ src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
@@ -1,22 +1,22 @@
 <?php
 
-namespace CSlant\Blog\Api\OpenApi\Schemas\Models;
+namespace CSlant\Blog\Api\OpenApi\Schemas\Resources\Category;
 
 use OpenApi\Attributes\Property;
 use OpenApi\Attributes\Schema;
 
 #[Schema(
-    schema: "Author",
-    required: ["id", "email"],
+    schema: "AuthorModelResource",
+    required: ["id"],
     properties: [
         new Property(property: "id", type: "integer", uniqueItems: true),
-        new Property(property: "email", description: "Author email", type: "string", maxLength: 191, nullable: true),
         new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120),
         new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120),
-        new Property(property: "username", description: "Author username", type: "string", maxLength: 60, uniqueItems: true),
+        new Property(property: "image", description: "Author image", type: "string", maxLength: 255),
+        new Property(property: "role", description: "Author role", type: "string", maxLength: 120),
     ],
     type: "object"
 )]
-class AuthorSchema
+class AuthorModelResourceSchema
 {
 }
