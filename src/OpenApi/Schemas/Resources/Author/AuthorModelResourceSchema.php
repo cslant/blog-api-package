f5ded67187dc22345add4939ee209c8cc5a116a3
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-12 10:37:08 +0700

    #44 feat: update required and set the required param as true

--- src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
+++ src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
@@ -7,12 +7,12 @@ use OpenApi\Attributes\Schema;
 
 #[Schema(
     schema: "AuthorModelResource",
-    required: ["id"],
+    required: ["id", "first_name", "last_name", "image"],
     properties: [
         new Property(property: "id", type: "integer", uniqueItems: true),
-        new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120),
-        new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120),
-        new Property(property: "image", description: "Author image", type: "string", maxLength: 255),
+        new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120, nullable: true),
+        new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120, nullable: true),
+        new Property(property: "image", description: "Author image", type: "string", maxLength: 255, nullable: true),
         new Property(property: "role", description: "Author role", type: "string", maxLength: 120),
     ],
     type: "object"
