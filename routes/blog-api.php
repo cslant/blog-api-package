db34029ed6add052a001f3aed739ef0169bac486
Xuan Thinh <pxthinh.vn@gmail.com>
2025-03-18 00:22:58 +0700

    #54 feat: add schema for author

--- routes/blog-api.php
+++ routes/blog-api.php
@@ -1,7 +1,7 @@
 <?php
 
-use CSlant\Blog\Api\Http\Controllers\Actions\Post\ViewCountAction;
-use CSlant\Blog\Api\Http\Controllers\Actions\Author\AuthorAction;
+use CSlant\Blog\Api\Http\Actions\Author\AuthorAction;
+use CSlant\Blog\Api\Http\Actions\Post\ViewCountAction;
 use CSlant\Blog\Api\Http\Controllers\CategoryController;
 use CSlant\Blog\Api\Http\Controllers\MetaBoxController;
 use CSlant\Blog\Api\Http\Controllers\PostController;
@@ -26,7 +26,7 @@ Route::prefix($routePrefix)->name("$routePrefix.")->middleware('api')->group(fun
 
     Route::get('search', [PostController::class, 'getSearch']);
 
-    Route::group(['prefix' => 'author'], function () {
+    Route::group(['prefix' => 'authors'], function () {
         Route::get('/{authorId}', AuthorAction::class);
     });
 
