64ade1c4943f634247c18222fb11cecaffed1a54
pxthinh <105281722+pxthinh@users.noreply.github.com>
2025-03-25 17:08:14 +0000

    Fix styling

--- src/Http/Actions/Auth/AuthPostRegisterAction.php
+++ src/Http/Actions/Auth/AuthPostRegisterAction.php
@@ -4,13 +4,13 @@ namespace CSlant\Blog\Api\Http\Actions\Auth;
 
 use Botble\Base\Http\Responses\BaseHttpResponse;
 use Botble\Member\Models\Member;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Hash;
 use CSlant\Blog\Core\Http\Actions\Action;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 /**
  * Class AuthPostRegisterAction
@@ -32,7 +32,7 @@ class AuthPostRegisterAction extends Action
             'first_name' => 'required|string',
             'last_name' => 'required|string',
             'email' => 'required|email|unique:members',
-            'password' => 'required|min:6|confirmed'
+            'password' => 'required|min:6|confirmed',
         ]);
 
         $token = Str::random(64);
