a1c57be5c566b6359bb462ad986010117afbd404
TonyDuong0509 <128566649+TonyDuong0509@users.noreply.github.com>
2025-03-27 08:49:12 +0000

    Fix styling

--- src/Services/VisitorLogsService.php
+++ src/Services/VisitorLogsService.php
@@ -44,12 +44,11 @@ class VisitorLogsService
             VisitorLog::create([
                 'viewable_id' => $entityId,
                 'viewable_type' => $entityType,
-                'ip_address' => $ipAddress
+                'ip_address' => $ipAddress,
             ] + $visitorData);
 
             $post->increment('views');
-        } elseif ($now->isAfter($existingView->expired_at))
-        {
+        } elseif ($now->isAfter($existingView->expired_at)) {
             $existingView->update($visitorData);
             $post->increment('views');
         }
