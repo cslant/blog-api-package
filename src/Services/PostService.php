0b296c8255017ad8b0c1649f12be52d8276cf095
FrancisDuong <haoda@cslant.com>
2025-03-27 16:50:03 +0700

    #53: fix: add namespace to PostService

--- src/Services/PostService.php
+++ src/Services/PostService.php
@@ -2,10 +2,13 @@
 
 namespace CSlant\Blog\Api\Services;
 
+use Botble\Base\Models\BaseQueryBuilder;
+use Botble\Language\Facades\Language;
 use CSlant\Blog\Core\Http\Responses\Base\BaseHttpResponse;
 use CSlant\Blog\Core\Models\Post;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
 /**
