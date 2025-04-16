fe18504a6b76c50405f9b2770fabf4e0abbc679a
Tan Nguyen <tannp27@gmail.com>
2025-04-16 09:19:34 +0700

    feat: add OpenAPI documentation for retrieving meta data by slug and model

--- src/Http/Actions/MetaBox/MetaBoxGetBySlugAction.php
+++ src/Http/Actions/MetaBox/MetaBoxGetBySlugAction.php
@@ -3,6 +3,7 @@
 namespace CSlant\Blog\Api\Http\Actions\MetaBox;
 
 use CSlant\Blog\Api\Http\Resources\MetaBox\MetaBoxResource;
+use CSlant\Blog\Api\OpenApi\Schemas\Resources\Post\PostListResourceSchema;
 use CSlant\Blog\Api\Services\MetaBoxService;
 use CSlant\Blog\Api\Services\SlugService;
 use CSlant\Blog\Core\Constants\AppConstant;
@@ -11,6 +12,13 @@ use CSlant\Blog\Core\Http\Responses\Base\BaseHttpResponse;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\Items;
+use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Parameter;
+use OpenApi\Attributes\Property;
+use OpenApi\Attributes\Response;
+use OpenApi\Attributes\Schema;
 
 /**
  * Class MetaBoxGetBySlugAction
@@ -35,6 +43,88 @@ class MetaBoxGetBySlugAction extends Action
         $this->slugService = $slugService;
     }
 
+    #[
+        Get(
+            path: "/meta-boxes/{model}/{modelSlug}/{lang?}",
+            operationId: "metaBoxGetBySlugModel",
+            description: "Get the meta data by slug and model.
+            
+    This API will get the meta SEO data by slug and model.
+    The model can be one of the following: post, page, category, tag, etc.
+            ",
+            summary: "Get meta data by slug and model",
+            tags: ["MetaBox"],
+            parameters: [
+                new Parameter(
+                    name: 'model',
+                    description: 'The model name. Can be one of the following: post, page, category, tag, etc.',
+                    in: 'path',
+                    required: true,
+                    schema: new Schema(
+                        type: 'string',
+                        default: 'post',
+                        enum: ['post', 'page', 'category', 'tag']
+                    )
+                ),
+                new Parameter(
+                    name: 'modelSlug',
+                    description: 'The slug of the model. Can be one of the following: post, page, category, tag, etc.
+                    
+    Example: post-slug, page-slug, category-slug, tag-slug, etc.',
+                    in: 'path',
+                    required: true,
+                    schema: new Schema(
+                        type: 'string',
+                        default: 'post-slug'
+                    )
+                ),
+                new Parameter(
+                    name: 'lang',
+                    description: 'The language code. Default is en.',
+                    in: 'path',
+                    required: false,
+                    schema: new Schema(
+                        type: 'string',
+                        default: AppConstant::DEFAULT_LOCALE
+                    )
+                ),
+            ],
+            responses: [
+                new Response(
+                    response: 200,
+                    description: "Get meta data by slug and model",
+                    content: new JsonContent(
+                        properties: [
+                            new Property(
+                                property: 'error',
+                                description: 'Error status',
+                                type: 'boolean',
+                                default: false
+                            ),
+                            new Property(
+                                property: "data",
+                                ref: MetaBoxResource::class,
+                                description: "Data of the meta box",
+                                type: "object",
+                            ),
+                        ]
+                    )
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\BadRequestResponseSchema::class,
+                    response: 400,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\ErrorNotFoundResponseSchema::class,
+                    response: 404,
+                ),
+                new Response(
+                    ref: \CSlant\Blog\Api\OpenApi\Responses\Errors\InternalServerResponseSchema::class,
+                    response: 500,
+                ),
+            ]
+        )
+    ]
     public function __invoke(string $model, string $slug, string $lang = AppConstant::DEFAULT_LOCALE)
     {
         $slugModel = $this->slugService->getSlugModel($slug, $model);
