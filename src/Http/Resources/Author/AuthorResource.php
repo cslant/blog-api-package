e414b4c8a56b3c2e8476d1ece8aa805a6222471b
Tan Nguyen <tannp27@gmail.com>
2025-04-20 19:02:25 +0700

    feat: add full_name property to Author model and resource representation

--- src/Http/Resources/Author/AuthorResource.php
+++ src/Http/Resources/Author/AuthorResource.php
@@ -22,6 +22,9 @@ class AuthorResource extends JsonResource
             'id' => $this->id,
             'first_name' => $this->first_name,
             'last_name' => $this->last_name,
+            'full_name' => $this->first_name && $this->last_name
+                ? $this->first_name . ' ' . $this->last_name
+                : ($this->first_name ?? $this->last_name ?? 'System User'),
             'image' => $this->avatar_url,
             'role' => $this->roles?->first()->name ?? null,
         ];
--- src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
+++ src/OpenApi/Schemas/Resources/Author/AuthorModelResourceSchema.php
@@ -12,6 +12,7 @@ use OpenApi\Attributes\Schema;
         new Property(property: "id", type: "integer", uniqueItems: true),
         new Property(property: "first_name", description: "Author first_name", type: "string", maxLength: 120, nullable: true),
         new Property(property: "last_name", description: "Author last_name", type: "string", maxLength: 120, nullable: true),
+        new Property(property: "full_name", description: "Author full_name", type: "string", maxLength: 255, nullable: true),
         new Property(property: "image", description: "Author image", type: "string", maxLength: 255, nullable: true),
         new Property(property: "role", description: "Author role", type: "string", maxLength: 120),
     ],
