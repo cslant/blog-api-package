1c7bacb772976b12f3db861ca7f2a7d5fa449706
Xuan Thinh <pxthinh.vn@gmail.com>
2025-04-23 23:28:15 +0700

    feat: add order by for custom filter post

--- src/Services/PostService.php
+++ src/Services/PostService.php
@@ -27,7 +27,7 @@ class PostService
      */
     public function getCustomFilters(array $filters): LengthAwarePaginator
     {
-        $data = Post::query();
+        $data = Post::query()->withCount(['comments', 'likes']);
 
         if ($filters['tags'] !== null) {
             $tags = array_filter((array) $filters['tags']);
