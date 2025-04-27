44217d3b4feaf92ebaa7781dfabdfd80c508929e
Tan Nguyen <tannp27@gmail.com>
2025-04-27 18:56:22 +0700

    refactor: update parameter type in FilterCategory for improved type safety

--- src/Supports/Filters/FilterCategory.php
+++ src/Supports/Filters/FilterCategory.php
@@ -5,7 +5,7 @@ namespace CSlant\Blog\Api\Supports\Filters;
 class FilterCategory
 {
     /**
-     * @param  array<string, mixed>  $request
+     * @param  array<string|mixed, mixed>  $request
      *
      * @return array<string, mixed>
      */
