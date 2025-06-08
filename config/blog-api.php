7da9081383af42354208f6739cec3432be47579f
Tan Nguyen <tannp27@gmail.com>
2025-06-08 17:37:48 +0700

    fix: add 'api' middleware to blog API route group for improved request handling

--- config/blog-api.php
+++ config/blog-api.php
@@ -5,6 +5,4 @@ return [
         /* Set route prefix for the blog API */
         'route_prefix' => env('BLOG_API_ROUTE_PREFIX', 'api'),
     ],
-
-    'cors_custom_domain_pattern' => env('CORS_CUSTOM_DOMAIN_PATTERN', '/^$/'),
 ];
